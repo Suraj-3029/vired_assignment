0f7439dca5165d920c8ef4574d887d9e1d03e45b
S-master-developer <suraj.mishra.saan@gmail.com>
2023-10-22 23:57:39 +0530

    wip

--- backup.py
+++
@@ -1,8 +0,0 @@
-import sys
-
-if (len(sys.argv) != 3):
-    print("Please give the Source and destination")
-    exit()
-source_dir = sys.argv[1]
-destination_dir = sys.argv[2]
-print(source_dir, destination_dir)
